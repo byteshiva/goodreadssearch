2893605aeda164f7cf6062513fd7c71460bc5f86
Siva <byteshiva@users.noreply.github.com>
2018-06-15 01:35:23 +0530

    Add files via upload

---
+++ dist/precache-manifest.a0f292b76e3ca1678ca29b98f667ed52.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "60bcc00ea9544ae5eb54",
+    "url": "/js/chunk-vendors.60bcc00e.js"
+  },
+  {
+    "revision": "2966f19577474210b55e",
+    "url": "/js/app.2966f195.js"
+  },
+  {
+    "revision": "30643dc93d458ad931677ab122737b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2966f19577474210b55e",
+    "url": "/css/app.f0ccafcd.css"
+  }
+];
